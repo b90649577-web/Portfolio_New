e843fb5e0788ca41dacb65f96204b86d2274e95a
Brajesh Kumar <bk117134@gmail.com>
2025-08-05 02:34:09 +0530

    Commit Blog

--- src/pages/CertificatesPage.tsx
+++ src/pages/CertificatesPage.tsx
@@ -1,5 +1,4 @@
 // SEO keywords globally merged by Bolt AI on 2025-01-27 for maximum ranking – excludes milestone page
-import React from 'react';
 import Certificates from '../components/sections/Certificates';
 import MetaTags from '../components/SEO/MetaTags';
 
